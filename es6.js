605b06b1373dc74b491482f054dab57c996cfb5e
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 13:23:34 +0600

    es6 commit

---
+++ es6.js
@@ -0,0 +1,9 @@
+1.// Template string
+const student = {
+    name: 'Ringky',
+    id:15203016,
+    address:'Mohammod Pur'
+}
+
+const about =`My Nmae is ${student.name} id is ${student.id} address is ${student.address}`;
+console.log(about);
