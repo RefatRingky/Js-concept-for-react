553205bfcf682e55c3d29fd38916ab93b88f4408
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 14:18:52 +0600

    destructuring js

---
+++ destructuring.js
@@ -0,0 +1,18 @@
+// Array Destruncturing
+const foo = ['one', 'two', 'three'];
+
+const [red, yellow, green] = foo;
+console.log(red); // "one"
+console.log(yellow); // "two"
+console.log(green); // "three"
+// Optional Chaining
+const adventurer = {
+    name: 'Alice',
+    cat: {
+      name: 'Dinah'
+    }
+  };
+  
+  const dogName = adventurer.dog?.name;
+  console.log(dogName);
+  console.log(adventurer.someNonExistentMethod?.());
