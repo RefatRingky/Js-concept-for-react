50188a7a8943bceff3674c672bb12063c7160b30
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 13:07:11 +0600

    fundamental js commit

--- fundamental.js
+++ fundamental.js
@@ -3,3 +3,27 @@ const fatherName = 'Arnold';
 let season = 'Rainy';
 season = 'winter';
 
+2.// Six basic condition >,<,==,===,!==,>=,<=
+// Multiple Condition && ,||
+if (fatherName === "Arnold" || season === "Rainy") {
+    
+}
+else if(fatherName === "Arnold"){
+
+}
+else{
+
+}
+
+3.// Array  declare in js
+// index,length,push,pop,shift,unshift,includes,indexOf
+const numbers = [34,78,90,65];
+numbers[0] = 77;
+
+4.// Loop in array
+for (let i = 0; i < number.length; i++) {
+    const number = number[i];
+    console.log(number);
+    
+}
+
