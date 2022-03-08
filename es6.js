71e382c1169c2146f64b321023bba0aea45997e8
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-08 13:35:55 +0600

    es6 commit

--- es6.js
+++ es6.js
@@ -7,3 +7,14 @@ const student = {
 
 const about =`My Nmae is ${student.name} id is ${student.id} address is ${student.address}`;
 console.log(about);
+
+// Arrow Function
+const getFiftyFive = () =>55;
+const addSixtyFive = num =>num+65;
+const multiply = (x,y,z) => x*y*z;
+
+// speard operator
+let numberStore = [0, 1, 2];
+let newNumber = (34);
+numberStore = [...numberStore, newNumber];
+console.log(numberStore);
